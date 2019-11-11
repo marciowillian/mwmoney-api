c75a5da24a4d4242ea391c3537b9b6597829fd10
Marcio Willian Chaves Cardoso <willian_mw.cc@hotmail.com>
2019-11-11 16:53:41 -0300

    Commit Inicial

---
+++ src/main/java/com/mwcc/mwccmoney/api/MwmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.mwcc.mwccmoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwmoneyApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mwcc/mwccmoney/api/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.mwcc.mwccmoney.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mwcc.mwccmoney.api.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+}
