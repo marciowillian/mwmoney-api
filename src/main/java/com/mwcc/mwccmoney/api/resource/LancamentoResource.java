71b2cb212ccaa7046686e9125896ffa05cf3f25b
Marcio Willian <willian_mw.cc@hotmail.com>
2020-02-24 23:52:14 -0300

    Aula 5.3 - Desafio - Cadastrando o primeiro lançamento

--- src/main/java/com/mwcc/mwccmoney/api/resource/LancamentoResource.java
+++ src/main/java/com/mwcc/mwccmoney/api/resource/LancamentoResource.java
@@ -1,12 +1,23 @@
 package com.mwcc.mwccmoney.api.resource;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mwcc.mwccmoney.api.event.RecursoCriadoEvent;
 import com.mwcc.mwccmoney.api.model.Lancamento;
 import com.mwcc.mwccmoney.api.repository.LancamentoRepository;
 
@@ -16,10 +27,29 @@ public class LancamentoResource {
 	
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<Lancamento> findAll(){
 		return lancamentoRepository.findAll();
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Lancamento> findById(@PathVariable Long codigo){
+		Optional<Lancamento> lancamentoSalvo = lancamentoRepository.findById(codigo);
+		
+		return !(lancamentoSalvo == null) ? ResponseEntity.of(lancamentoSalvo) : ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response){
+		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
 	}
 	
 }
